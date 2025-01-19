c7eb187a16a103bee74ec041a7cecb1c2dca53cc
Guido La Rosa <guido.glarosa@gmail.com>
2025-01-19 12:24:46 -0300

    chore: fixed type issue

---
+++ app/types/react-splide.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '@splidejs/react-splide' {
+  import { ComponentType } from 'react';
+
+  export const Splide: ComponentType<any>;
+  export const SplideSlide: ComponentType<any>;
+}
