2f57eb00347a2d04b9e03a54afadf615702ddc37
Guido La Rosa <guido.glarosa@gmail.com>
2025-01-20 03:57:40 -0300

    chore: udpated styles

--- app/components/layout/AuroraEffect.tsx
+++ app/components/layout/AuroraEffect.tsx
@@ -2,23 +2,18 @@ import React from "react";
 import "@/app/styles/Aurora.css"; // Import the CSS file
 import clsx from "clsx";
 
-const AuroraEffect = ({ className } : { className: string }) => {
+const AuroraEffect = ({ className }: { className: string }) => {
   return (
     <article
       id="wrap"
       className={clsx(className, "absolute w-screen h-screen")}
     >
-      <article id="lightings">
-        <section id="one">
-          <section id="two">
-            <section id="three">
-              <section id="four">
-                <section id="five"></section>
-              </section>
-            </section>
-          </section>
-        </section>
-      </article>
+      <section className="wrapper">
+        <div className="hero"></div>
+        <div className="content">
+          <input type="checkbox" id="switch" checked/>
+        </div>
+      </section>
     </article>
   );
 };
