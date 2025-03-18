c348f4e7aadffed3d41fd2a224557659e0fbf0e3
Guido La Rosa <guido.glarosa@gmail.com>
2025-03-18 17:59:32 -0300

    chore: updated text and sizes

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -13,6 +13,7 @@ import {
   // IMAGE,
   // SINGLE_IMAGE_CONTAINER,
 } from "../styles/common";
+import Link from "next/link";
 
 export default async function Team() {
   // const client = createClient();
@@ -35,46 +36,42 @@ export default async function Team() {
           <h1 className={H1_STYLE}>¿Qué es Crows Nest?</h1>
           <div className={P_STYLE}>
             <p>
-              Crows Nest PCG es una empresa dedicada al{" "}
-              <strong>color grading</strong> y{" "}
-              <strong>post producción de color</strong> en todas sus etapas,
-              desde el{" "}
-              <strong>asesoramiento en pre-producción (¨look book¨)</strong>,
-              pasando por <strong>la supervisión en rodaje</strong> hasta el{" "}
-              <strong>mastering final</strong> de la obra, creando y diseñando
-              el flujo de trabajo particular de cada proyecto, desde los ¨in
-              house¨, hasta trabajos internacionales en forma remota.
+              Crows Nest PCG es una empresa especializada en color grading y
+              postproducción de color en todas sus etapas.
             </p>
             <p>
-              Largometrajes, series, comerciales, cortometrajes, videoclips y
-              trabajos para TV y web son nuestra tarea de cada día,{" "}
-              <strong>todos los días</strong>.
+              Ofrecemos asesoramiento en preproducción (look book), supervisión
+              en rodaje y mastering final, diseñando un flujo de trabajo
+              personalizado para cada proyecto. Trabajamos tanto en estudios
+              propios (in-house) como en producciones internacionales de forma
+              remota.
             </p>
             <p>
-              Contamos con{" "}
-              <strong>
-                20 años de experiencia en diseño y asesoramiento para salas de
-                Color Grading profesionales a productoras y casas de
-                post-producción
-              </strong>
-              , considerando la ambientación técnica adecuada; desde la pintura
-              de la sala, pasando por la iluminación, los muebles, y hasta el
-              hardware necesario según el tipo de trabajos que se deseen
-              afrontar.
+              Nos dedicamos al color grading para largometrajes, series,
+              comerciales, cortometrajes, videoclips y contenidos para TV y web,
+              brindando soluciones de alta calidad día tras día.
             </p>
             <p>
-              Somos los{" "}
-              <strong>
-                primeros en utilizar, desarrollar y actualizar equipos
-                profesionales de corrección de color
-              </strong>{" "}
-              y armar salas de color grading profesional en el país.
+              Con más de 20 años de experiencia, asesoramos y diseñamos salas de
+              color grading profesionales para productoras y casas de
+              postproducción.
+            </p>
+            <p>
+              Nos enfocamos en cada detalle técnico, desde la pintura y la
+              iluminación hasta el mobiliario y el hardware necesario según las
+              exigencias de cada proyecto. Fuimos pioneros en el uso, desarrollo
+              y actualización de equipos profesionales de corrección de color,
+              así como en la creación de salas de color grading de nivel
+              profesional en el país. Gracias a nuestra experiencia y
+              conocimiento acumulados a lo largo de los años, también ofrecemos
+              cursos personalizados, adaptados a las necesidades específicas de
+              cada cliente.
             </p>
           </div>
         </div>
         <div className="flex flex-col gap divide-y divide-stone-700">
-          <div className="flex gap-6 py-10">
-            <div className="relative min-w-40 h-40 aspect-square rounded-full overflow-hidden">
+          <div className="flex flex-col md:flex-row gap-6 py-10">
+            <div className="relative min-w-40 w-40 h-40 aspect-square rounded-full overflow-hidden">
               <Image
                 priority
                 src="/images/laura.jpg"
@@ -86,27 +83,57 @@ export default async function Team() {
             <div>
               <h2 className={H2_STYLE}>Laura Viviani</h2>
               <p>
-                Pionera en Intermedio Digital en el país, y prolífera colorista
-                de cine digital con infinidad de producciones cinematográficas y
-                publicitarias en su currículum. Es socia fundadora de la
-                Asociación Argentina de Coloristas Audiovisuales (AAC).
+                Colorista Senior con una amplia trayectoria en producciones
+                cinematográficas y publicitarias.
               </p>
+
               <p>
-                Es la instructora oficial de Assimilate Scratch en la Argentina
-                y docente de Manejo del Color en Postproducción en la ENERC.
+                Forma parte del medio audiovisual desde finales de los años 90 y
+                fue pionera en Intermedio Digital en Argentina, trabajando en
+                Metrovisión Producciones.
               </p>
+
               <p>
-                También dicta seminarios en el interior del país y Uruguay
-                .Estudió diseño de Imagen y Sonido UBA y es socia fundadora de
-                la escuela PuntoCINE, institución de formación de grado y
-                postgrado interdisciplinaria en las áreas de realización,
-                postproducción y animación, desarrolladas por profesionales en
-                actividad del medio.
+                Gracias a su conocimiento en diversas áreas, ha participado en
+                múltiples producciones desempeñando distintos roles, incluyendo
+                fotografía, cámara, VFX y producción.
               </p>
+
+              <p>
+                Es socia fundadora de la Asociación Argentina de Coloristas
+                Audiovisuales (AAC) y de la escuela PuntoCINE.
+              </p>
+
+              <p>
+                Estudió Diseño de Imagen y Sonido en la UBA y ha realizado
+                cursos de actualización en diversas instituciones como FIAF,
+                FUNDAVIS, UNA, BMD, etc., con el objetivo de abordar el
+                conocimiento del color desde una perspectiva técnica, física,
+                corporal y emocional.
+              </p>
+
+              <p>
+                Es instructora oficial de Assimilate Scratch en Argentina y
+                docente de Manejo del Color en Postproducción en la ENERC y
+                PuntoCINE.
+              </p>
+
+              <p>
+                Además, imparte seminarios y charlas en festivales, empresas y
+                para particulares en Argentina y Uruguay.
+              </p>
+
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/laura-viviani-color/
+"
+              >
+                LinkedIn
+              </Link>
             </div>
           </div>
-          <div className="flex gap-6 py-10">
-            <div className="relative min-w-40 h-40 aspect-square rounded-full overflow-hidden">
+          <div className="flex flex-col md:flex-row gap-6 py-10">
+            <div className="relative min-w-40 w-40 h-40 aspect-square rounded-full overflow-hidden">
               <Image
                 src="/images/edu_sierra.webp"
                 fill
@@ -118,44 +145,79 @@ export default async function Team() {
               <h2 className={H2_STYLE}>Eduardo Sierra</h2>
               <p>
                 Es el colorista con mayor experiencia profesional en actividad
-                de la Argentina. Su experiencia inicial fue en laboratorio
-                cinematográfico y es uno de los primeros coloristas en transfer
-                de película a video en Argentina; su trabajo se combina entre su
-                desempeño en Intermedio Digital (DI ) y las últimas tecnologías
-                de corrección de color de cinematografía digital. Su currículum
-                posee innumerables realizaciones cinematográficas y
-                publicitarias.
+                de Argentina.
               </p>
+
               <p>
-                Fundador de Cinefoto, el primer medio digital sobre divulgación
-                tecnológica e intercambio de experiencias profesionales de
-                Argentina.
+                Su carrera comenzó en el laboratorio cinematográfico Cinecolor
+                en 1984, siendo uno de los primeros coloristas en realizar
+                transferencias de película a video en el país.
               </p>
+
+              <p>
+                Su trabajo abarca tanto su desempeño en Intermedio Digital (DI)
+                como las últimas tecnologías de corrección de color en
+                cinematografía digital.
+              </p>
+
+              <p>
+                Su currículum incluye innumerables proyectos cinematográficos y
+                publicitarios.
+              </p>
+
+              <p>
+                Además, está diplomado en electrónica, fotografía fija
+                profesional y dirección de fotografía cinematográfica.
+              </p>
+
+              <p>
+                Es también moderador y fundador del foro Cinefoto, el primer
+                medio digital de divulgación tecnológica e intercambio de
+                experiencias profesionales en Argentina.
+              </p>
+
               <p>
                 Es socio fundador de la Asociación Argentina de Coloristas
-                Audiovisuales(AAC) y además miembro de la Color Society
-                Internacional (CSI), la Society of Motion Pictures and
-                Television Ingenieers (SMPTE), Independient Colorist Guild (ICG)
-                y la Asociación de Autores de Fotografía Cinematográfica de
-                Argentina (ADF).
+                Audiovisuales (AAC) y miembro de la Color Society International
+                (CSI), Independent Colorist Guild (ICG), la Society of Motion
+                Picture and Television Engineers (SMPTE) y la Asociación de
+                Autores de Fotografía Cinematográfica de Argentina (ADF).
               </p>
+
               <p>
-                Su experiencia docente está avalada por su actividad en CFP
-                SICA, ENERC, FUC y seminarios dictados en UNA, UNT, EPCTV,
-                UNICEN, UNLP lo que también lo llevó a formar su propia escuela
-                de especialización en postproducción cinematográfica: PuntoCine.
+                Su experiencia docente está respaldada por su participación en
+                instituciones como CFP SICA, ENERC, FUC, PUNTOCINE, así como en
+                seminarios dictados en UNA, UNT, EPCTV, UNICEN, UNLP y TGSM.
               </p>
+
               <p>
-                En el exterior ha dictado Cursos de Scratch, Da Vinci Resolve y
-                Color Grading en Pixelovers de Chile, en el TOA y la Universidad
-                Católica del Uruguay. Mantiene ademas su actividad profesional
-                como consultor de color y Colorista Senior en Metrovisión S.A. Y
-                Cinecolor Digital.
+                Esta experiencia le permitió fundar su propia escuela de
+                especialización en postproducción cinematográfica: PuntoCine -
+                Buenos Aires.
               </p>
+
+              <p>
+                En el exterior, ha dictado cursos de Scratch, DaVinci Resolve y
+                Color Grading en Pixelovers (Chile), en el TOA y en la
+                Universidad Católica del Uruguay.
+              </p>
+
+              <p>
+                Además de su labor docente, mantiene una activa carrera
+                profesional como consultor de color y Colorista Senior en
+                Metrovisión Producciones y Cinecolor Digital.
+              </p>
+
+              <Link
+                target="_blank"
+                href="https://www.imdb.com/name/nm4096874/?ref_=nv_sr_srsg_1_tt_0_nm_8_in_0_q_Eduardo%2520Sierra"
+              >
+                IMDB
+              </Link>
             </div>
           </div>
-          <div className="flex gap-6 py-10">
-            <div className="relative min-w-40 h-40 aspect-square rounded-full overflow-hidden">
+          <div className="flex flex-col md:flex-row gap-6 py-10">
+            <div className="relative min-w-40 w-40 h-40 aspect-square rounded-full overflow-hidden">
               <Image
                 src="/images/mariano.JPG"
                 fill
@@ -166,25 +228,68 @@ export default async function Team() {
             <div>
               <h2 className={H2_STYLE}>Fernando Rivas</h2>
               <p>
-                Colorista referente del departamento Color Grading de
-                Telefe/VIACOM Argentina, formador de los coloristas de la
-                empresa, e instructor de Davinci Resolve y docente del curso de
-                Cine Digital en el centro de formación y laboratorio PuntoCINE.
-                Es socio fundador de la Asociación Argentina de Coloristas
-                Audiovisuales (AAC). Como docente también ha dictado talleres y
-                cursos de color en la Universidad de las Americas (Chile), en el
+                Coordinador del Departamento de Color Grading del área de
+                postproducción de PARAMOUNT/TELEFE, formador de los coloristas
+                de la empresa y encargado de las charlas de capacitación del
+                personal interno de las distintas áreas.
+              </p>
+
+              <p>
+                También es asesor de las tecnologías necesarias de las salas de
+                grading y del diseño y testeo de los flujos para los diferentes
+                desafíos propuestos, así como del QC interno y de soporte para
+                otras áreas técnicas.
+              </p>
+
+              <p>
+                Instructor Certificado de BMD Davinci Resolve y docente de
+                cursos de Color con Resolve y de Cine Digital en el centro de
+                formación y laboratorio PuntoCINE.
+              </p>
+
+              <p>
+                Como docente, también ha dictado talleres y cursos de color
+                desde 2014 en la Universidad de las Américas (Chile), en el
                 Taller de Oficios Audiovisuales del Uruguay (TOA) y en la UNICEN
                 (Universidad del Centro, Tandil).
               </p>
+
               <p>
-                Cuenta con amplia experiencia tanto en cine como en TV y demas
-                flujos de color grading actuales, como asi tambien en el montaje
-                de departamentos técnicos de empresas líderes en postproducción
-                de la Argentina como Metrovision, es tambien pionero en el
-                manejo de la tecnologia HDR de la Argentina. Trabajó como
-                consultor y colorista de post productoras como La Posta, UFA!
-                Post y RGB Entertainment entre otros.
+                Ha realizado capacitaciones sobre corrección de color para
+                instituciones privadas como el Teatro Gral. San Martín, Viditec
+                S. A. y Aleta Media, entre otros.
               </p>
+
+              <p>
+                Es socio fundador de la Asociación Argentina de Coloristas
+                Audiovisuales (AAC) y forma parte de la Comisión de Formación
+                Profesional de la misma.
+              </p>
+
+              <p>
+                Cuenta con amplia experiencia tanto en cine y publicidad como en
+                TV y demás flujos de color grading actuales, así como en el
+                montaje de departamentos técnicos y salas de Color Grading en
+                empresas líderes de postproducción en Argentina, como
+                Metrovisión.
+              </p>
+
+              <p>
+                Es también pionero en el manejo de la tecnología HDR en
+                Argentina.
+              </p>
+
+              <p>
+                Trabajó como consultor y colorista en postproductoras como La
+                Posta, UFA! Post y RGB Entertainment, entre otras.
+              </p>
+
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/fernandogabrielrivas/"
+              >
+                LinkedInd
+              </Link>
             </div>
           </div>
         </div>
